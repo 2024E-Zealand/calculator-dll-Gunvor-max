3b817ac48bf204a96704067e0aca572e0a6564ec
Mathias Birkkjær Nielsen <birkkjaer6@gmail.com>
2024-09-16 13:53:11 +0200

    Calculator with addition complete

---
+++ CalculatorClassLibrary/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorClassLibrary
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+            
+        }
+
+        /// <summary>
+        /// Calculate addition
+        /// </summary>
+        /// <param name="x">First number</param>
+        /// <param name="y">Second number</param>
+        /// <returns>Sum of first and second number</returns>
+        public double CalculateAddition(double x, double y)
+        {
+            return x + y;
+        } 
+    }
+}
---
+++ CalculatorClassLibraryTests/CalculatorTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorClassLibrary.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        Calculator TestCalculator { get; set; }
+        [TestInitialize]
+        public void beforeeachtest()
+        {
+            //Arrange
+            Calculator calculator = new Calculator();
+            TestCalculator = calculator;
+        }
+
+
+        [TestMethod()]
+        [DataRow(1,1,2)]
+        public void CalculateAdditionTest(double x, double y, double result)
+        {
+            //Act
+            var expectedResult = result;
+            var actualResult = TestCalculator.CalculateAddition(x, y);
+            //Assert
+            Assert.AreEqual(expectedResult,actualResult);
+        }
+    }
+}
